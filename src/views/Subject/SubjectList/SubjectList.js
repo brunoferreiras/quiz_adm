662d435b42de6e25eedb2d86e28bb93cf813cbbb
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-02-09 21:53:10 -0300

    Add redux and new in SubjectList

--- src/views/Subject/SubjectList/SubjectList.js
+++ src/views/Subject/SubjectList/SubjectList.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import {
+    Row,
+    Col,
+    Card,
+    CardHeader,
+    CardBody,
+    Table
+} from 'reactstrap';
+import { connect } from 'react-redux';
 
 class SubjectList extends Component {
     constructor(props){
@@ -7,9 +16,48 @@ class SubjectList extends Component {
 
     render() {
         return (
-            <h1>Subject List</h1>
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" md="12">
+                        <Card>
+                            <CardHeader>
+                                Listing
+                            </CardHeader>
+                            <CardBody>
+                                <a href="/subject/new" className="btn btn-primary mb-2">Create new subject</a>
+                                <Table responsive>
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Puzzle</td>
+                                            <td>01/01/2000</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
 
-export default SubjectList;
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubjectList);
